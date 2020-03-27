45d5a70a024de9a75e6dccc3ad2681649aa2bb86
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-27 16:38:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,10 +34,6 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
-    if self.create_by_name(name) == nil 
-      self.create_by_name(name)
-    else 
-      self.find_by_name(name)
-    end 
+    self.find_by_name(name) || self.create_by_name(name)
   end 
 end
