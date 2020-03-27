385807911bb6779ecb24b3f77024efac0ef93b16
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-27 16:43:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,5 +44,6 @@ class Song
   def self.new_from_filename(name)
     song = self.new 
     song.name = (name.split(" ")[1].chomp(".mp3"))
+    song.artist_name = (artist_name.split("-"))
   end 
 end
